c5dcf3ca734650892747645372afe6a026b550cb
James Batsell <jamesbatsell05@gmail.com>
2020-02-14 01:21:51 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,18 +1,34 @@
 class TicTacToe
-  def initialize
-    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
-  end
-  
   WIN_COMBINATIONS = [
     [0,1,2], 
     [3,4,5], 
-    [6,7,8]
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [6,4,2]
+    
     
     ]
   
   
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+end
+  
+  
   def display_board
-    print @board
+      
+  print " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+  print  "-----------"
+  print " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+  print "-----------"
+  print " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+  
+  def move(index, token="X")
+    @board = token
   end
   
 end
